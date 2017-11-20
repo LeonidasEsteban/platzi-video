093a5a4c93037efd3f50e9b38d58203089584979
Leonidas Esteban <leonidasesteban@gmail.com>
2017-11-20 16:20:50 -0500

    clase 8 estilos en react

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
+import './media.css';
 
 class Media extends Component {
   render() {
+    const styles = {
+      container: {
+        color: '#44546b',
+        cursor: 'pointer',
+        width: 260,
+        border: '1px solid red'
+      }
+    }
     return (
-      <div>
-        <div>
+      <div className="Media">
+        <div className="Media-cover">
           <img
-            src=""
+            src="./images/covers/responsive.jpg"
             alt=""
             width={260}
             height={160}
+            className="Media-image"
           />
-          <h3>¿Por qué aprender React?...</h3>
-          <p>LeonidasEsteban</p>
+          <h3 className="Media-title">Que es responsive design</h3>
+          <p className="Media-author">LeonidasEsteban</p>
         </div>
       </div>
     )
   }
 }
+
+
 
 export default Media;
 
