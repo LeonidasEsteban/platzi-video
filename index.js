77c135cfdd33c6560fdb71de07da0ff03d885dfb
Leonidas Esteban <leonidasesteban@gmail.com>
2017-11-20 14:57:47 -0500

    clase 6 - react dom

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hola mundo!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// console.log('Hola mundo!')
+
+const app = document.getElementById('app')
+
+// ReactDOM.render(que voy a renderizar, donde lo haré);
+const holaMundo = <h1>hola Estudiante!</h1>;
+ReactDOM.render(holaMundo , app);
+
