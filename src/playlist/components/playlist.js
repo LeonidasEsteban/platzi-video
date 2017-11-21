e77d0e41c11172ba692f1fc6c65b320252ecb41d
Leonidas Esteban <leonidasesteban@gmail.com>
2017-11-21 11:15:46 -0500

    clase 15 - pure component y func component

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Media from './media.js';
 import './playlist.css';
 
-class Playlist extends Component {
-  render() {
-    const playlist = this.props.data.categories[0].playlist
-    console.log(this.props.data);
-    return (
-      <div className="Playlist">
-        {
-          playlist.map((item) => {
-            return <Media {...item} key={item.id} />
-          })
-        }
-      </div>
-    )
-  }
+
+
+function Playlist(props) {
+  const playlist = props.data.categories[0].playlist
+  console.log(props.data);
+  return (
+    <div className="Playlist">
+      {
+        playlist.map((item) => {
+          return <Media {...item} key={item.id} />
+        })
+      }
+    </div>
+  )
 }
 
 export default Playlist;
