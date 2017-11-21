d5164bba6b8686e9d2a1a60a9d2679fd6e4c2e10
Leonidas Esteban <leonidasesteban@gmail.com>
2017-11-21 16:33:07 -0500

    clase 25 - manejando formularios

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -6,12 +6,19 @@ import './search.css';
 //   )
 // }
 
-const Search = () => (
-  <form className="Search">
+const Search = (props) => (
+  <form
+    className="Search"
+    onSubmit={props.handleSubmit}
+  >
     <input
+      ref={props.setRef}
       type="text"
       placeholder="Busca tu video favorito"
       className="Search-input"
+      name="search"
+      onChange={props.handleChange}
+      value={props.value}
     />
   </form>
 )
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 import Search from '../components/search';
 
 class SearchContainer extends Component {
+  state = {
+    value: 'Luis Fonsi'
+  }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.input.value, 'submit')
+
+  }
+  setInputRef = element => {
+    this.input = element;
+  }
+  handleInputChange = event => {
+    this.setState({
+      value: event.target.value.replace(' ', '-')
+    })
+  }
   render() {
     return (
-      <Search />
+      <Search
+        setRef={this.setInputRef}
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handleInputChange}
+        value={this.state.value}
+      />
     )
   }
 }
